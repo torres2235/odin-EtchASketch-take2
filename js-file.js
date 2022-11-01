e3a460efd470216d763ef8a846d5ed02e1c18cea
Torres2235 <Torres2235@gmail.com>
2022-11-01 15:34:21 -0700

    adding 'clear' fucntionality

--- js-file.js
+++ js-file.js
@@ -6,7 +6,7 @@ for(let i = 0; i < 256; i ++) {
     //create each square for our grid
     let div = document.createElement('div');
     div.classList.add('square');
-    div.textContent = '.';
+    //div.textContent = '.';
     //setting size of each square to be (in this case 1/16)
     div.style.width = 'calc(100%/16)';
     div.style.height = 'calc(100%/16)';
@@ -14,3 +14,22 @@ for(let i = 0; i < 256; i ++) {
     //append each square to our grid
     grid.appendChild(div);
 }
+
+//select all the grid divs
+document.querySelectorAll('.square').forEach(item => {
+    //add the mouseover event to each
+    item.addEventListener('mouseover', function (e) {
+        //have that div change black when hovered
+        e.target.style.background = 'black';
+    });
+})
+
+
+
+//adds clearing functionality
+let clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+    document.querySelectorAll('.square').forEach(item => {
+        item.style.background = 'white';
+    })
+})
