f76267b6d910e3e8fb833fbbeb152ff3f1422598
Torres2235 <Torres2235@gmail.com>
2022-11-03 11:08:53 -0700

    adding resize()

--- js-file.js
+++ js-file.js
@@ -1,19 +1,27 @@
 let body = document.querySelector('body');
 let grid = document.querySelector('#grid');
 
-//16x16 grid = 256 squares
-for(let i = 0; i < 256; i ++) {
-    //create each square for our grid
-    let div = document.createElement('div');
-    div.classList.add('square');
-    //div.textContent = '.';
-    //setting size of each square to be (in this case 1/16)
-    div.style.width = 'calc(100%/16)';
-    div.style.height = 'calc(100%/16)';
+//on loading the js, run the resize function
+document.body.onload = resize(16);
 
-    //append each square to our grid
-    grid.appendChild(div);
+function resize(x) {
+    let totalSquares = x * x;
+
+    //16x16 grid = 256 squares
+    for(let i = 0; i < totalSquares; i ++) {
+        //create each square for our grid
+        let div = document.createElement('div');
+        div.classList.add('square');
+        //div.textContent = '.';
+        //setting size of each square to be (in this case 1/16)
+        div.style.width = 'calc(100%/' + x +')';
+        div.style.height = 'calc(100%/' + x + ')';
+
+        //append each square to our grid
+        grid.appendChild(div);
+    }
 }
+
 
 //select all the grid divs
 document.querySelectorAll('.square').forEach(item => {
@@ -23,8 +31,6 @@ document.querySelectorAll('.square').forEach(item => {
         e.target.style.background = 'black';
     });
 })
-
-
 
 //adds clearing functionality
 let clear = document.querySelector('#clear');
