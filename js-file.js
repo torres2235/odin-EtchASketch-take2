239e58d4c644a73baa172c5fd8195b92bbe62019
Torres2235 <Torres2235@gmail.com>
2022-11-03 11:29:55 -0700

    adding resizing functionality

--- js-file.js
+++ js-file.js
@@ -2,9 +2,9 @@ let body = document.querySelector('body');
 let grid = document.querySelector('#grid');
 
 //on loading the js, run the resize function
-document.body.onload = resize(16);
+document.body.onload = resize();
 
-function resize(x) {
+function resize(x=16) {
     let totalSquares = x * x;
 
     //16x16 grid = 256 squares
@@ -20,8 +20,15 @@ function resize(x) {
         //append each square to our grid
         grid.appendChild(div);
     }
+    //select all the grid divs
+    document.querySelectorAll('.square').forEach(item => {
+        //add the mouseover event to each
+        item.addEventListener('mouseover', function (e) {
+            //have that div change black when hovered
+            e.target.style.background = 'black';
+        });
+    })
 }
-
 
 //select all the grid divs
 document.querySelectorAll('.square').forEach(item => {
@@ -39,3 +46,28 @@ clear.addEventListener('click', () => {
         item.style.background = 'white';
     })
 })
+
+//resizes our grid
+let form = document.querySelector('form');
+
+form.addEventListener("submit", function(e){
+
+    e.preventDefault(); //prevents autosubmiting
+
+    //creates an array of each div in the container
+    let children = document.getElementById("grid").getElementsByTagName("*");
+
+    //cycles through each div
+    for( var i = 0; i<children.length;){ 
+        children[i].remove();
+    }
+
+    let size = document.getElementById('size').value
+    
+    if(size <= 100) {
+        resize(size);
+    } else {
+        alert("Invalid size");
+        resize(16);
+    } 
+});
